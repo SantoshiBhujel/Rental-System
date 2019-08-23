2705c53cbab09aec13c69a36c306a13c4b6cfa23
ParasKatwal <katwal.paras@gmail.com>
2019-08-23 20:12:33 +0545

    updated

--- include/header.php
+++ include/header.php
@@ -1,10 +1,51 @@
+
+
 <!DOCTYPE html>
 <html lang="en">
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>Document</title>
+    <title><?php echo Title ?></title>
+    <link rel="stylesheet" href="css/style.css">
+    <!-- Bootstrap CSS -->
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
 </head>
 <body>
+
+<nav class="navbar navbar-expand-lg navbar-light bg-light">
+  <a class="navbar-brand" href="#"><img src="img/logo2.png" alt=""></a>
+  <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+    <span class="navbar-toggler-icon"></span>
+  </button>
+
+  <div class="collapse navbar-collapse" id="navbarSupportedContent">
+    <ul class="navbar-nav mr-auto">
+      <li class="nav-item">
+        <a class="nav-link" href="#">HOME<span class="sr-only">(current)</span></a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link" href="#">RENT</a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link" href="#">CONTACT US</a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link" href="#">ABOUT US</a>
+      </li>
+      <li class="nav-item dropdown">
+        <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+          SIGN-IN/UP
+        </a>
+        <div class="dropdown-menu" aria-labelledby="navbarDropdown">
+          <a class="dropdown-item" href="#">SIGN IN</a>
+          <a class="dropdown-item" href="#">SIGN UP</a>
+      </li>
+    </ul>
+    <form class="form-inline my-2 my-lg-0">
+      <input class="form-control mr-sm-2" type="search" placeholder="Location" aria-label="Search">
+      <button class="btn btn-outline-success my-2 my-sm-0" type="submit">Search</button>
+    </form>
+  </div>
+</nav>
     
--- index.php
+++ index.php
@@ -1,7 +1,19 @@
+
+<?php 
+    define("Title","EASYRENT");
+?> 
 <?php include('include/header.php'); ?>
 
-<div>
-    <h1>hello handsome</h1>
+<div class="top-section">
+    <div class="top-video embed-responsive embed-responsive-16by9">
+        <video src="video/Retro-Sunset.mp4" class="embed-responsive-item" allowfullscreen autoplay loop muted></video>
+    </div>
+    <div class="working">
+        <h1>How it works</h1>
+        <a href="#">Post rents</a>
+        <a href="#">Search rents</a>
+        <a href="#">Make a deal</a>
+    </div>
 </div>
 
 <?php include('include/footer.php'); ?>
